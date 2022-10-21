349f3eb2ea1da62d2ec5fba52fa432e2cf04cf0b
sagishaked <85248427+sagishaked@users.noreply.github.com>
2022-10-21 12:36:32 +0300

    Update home_test_2_sagi_shaked.py

--- home_test_2_sagi_shaked.py
+++ home_test_2_sagi_shaked.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/flight_info/<string:flight_id>', methods=['GET'])
 def return_info(flight_id):
-    flights_data = pd.read_csv('flights_1.csv')
+    flights_data = pd.read_csv('after_q1_flights.csv')
     d = flights_data[flights_data['flight ID'] == flight_id].to_dict(orient='index')
     if len(d) == 0:
         ret_info = jsonify({flight_id: "flight ID does not exist"})
@@ -26,11 +26,11 @@ def add_flight():
     if len(missed_keys) != 0:
         return jsonify({'missing flight details': missed_keys})
     else:
-        flights_data = pd.read_csv('flights_1.csv')
+        flights_data = pd.read_csv('after_q1_flights.csv')
         new_flight = {'flight ID': request.json['flight ID'], 'Arrival': request.json['Arrival'],
                       'Departure': request.json['Departure'], 'success': request.json['success']}
         flights_data = flights_data.append(new_flight, ignore_index=True)
-        flights_data.to_csv('flights_1.csv', index=False)
+        flights_data.to_csv('after_q1_flights.csv', index=False)
 
         return jsonify({'flights': [flights_data.to_dict()]})
 
@@ -38,12 +38,12 @@ def add_flight():
 
 @app.route('/remove_flight/<string:flight_id>', methods=['POST'])
 def remove_flight(flight_id):
-    flights_data = pd.read_csv('flights_1.csv')
+    flights_data = pd.read_csv('after_q1_flights.csvv')
     flights_data_after_drop = flights_data.loc[flights_data["flight ID"] != flight_id]
     if flights_data.equals(flights_data_after_drop):
         return jsonify({flight_id: 'no flight id to remove'})
     else:
-        flights_data_after_drop.to_csv('flights_1.csv', index=False)
+        flights_data_after_drop.to_csv('after_q1_flights.csv', index=False)
         return jsonify({'flights': [flights_data_after_drop.to_dict()]})
 
 
